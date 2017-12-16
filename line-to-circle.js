83a9ced493ed90a855c78000ca1bd180c57637b7
Anwesh43 <anweshthecool0@gmail.com>
2017-12-17 00:11:31 +0530

    adding style changes

--- line-to-circle.js
+++ line-to-circle.js
@@ -2,12 +2,12 @@ const r = Math.min(window.innerWidth,window.innerHeight)/3
 class LineToCirclePoint {
     constructor(deg) {
         this.deg = deg
-        this.x = r*Math.cos(this.deg*Math.PI/180)
-        this.y = r*Math.sin(this.deg*Math.PI/180)
+        this.x = (r/2)*Math.cos(this.deg*Math.PI/180)
+        this.y = (r/2)*Math.sin(this.deg*Math.PI/180)
     }
     setXY() {
-        this.x = r*Math.cos(this.deg*Math.PI/180)
-        this.y = r*Math.sin(this.deg*Math.PI/180)
+        this.x = (r/2)*Math.cos(this.deg*Math.PI/180)
+        this.y = (r/2)*Math.sin(this.deg*Math.PI/180)
     }
     update() {
         if(this.deg < 360) {
@@ -15,7 +15,7 @@ class LineToCirclePoint {
             this.setXY()
         }
         else {
-            this.x += (2*Math.PI*r)/360
+            this.x += (Math.PI*r)/360
         }
     }
 }
@@ -33,6 +33,8 @@ class LineToCircle {
         context.save()
         context.translate(r,r)
         context.lineWidth = 5
+        context.strokeStyle = '#6A1B9A'
+        context.lineCap = 'round'
         context.beginPath()
         this.points.forEach((point,index)=>{
             if(index == 0) {
@@ -66,6 +68,11 @@ class Stage {
     draw() {
         this.context.clearRect(0,0,this.canvas.width,this.canvas.height)
         this.lineToCircle.draw(this.context)
+        this.context.lineWidth = 4
+        this.context.beginPath()
+        this.context.moveTo(r+r/2,2*r-r/10)
+        this.context.lineTo(r+r/2+r*Math.PI,2*r-r/10)
+        this.context.stroke()
     }
     update(stopcb) {
         this.lineToCircle.update(stopcb)
@@ -80,7 +87,7 @@ class Looper {
             this.animated = true
             this.interval = setInterval(()=>{
                 startcb()
-            },50)
+            },20)
         }
     }
     stop() {
@@ -91,6 +98,7 @@ class Looper {
     }
 }
 const stage = new Stage()
+stage.draw()
 const looper = new Looper()
 window.onmousedown = (event) => {
     looper.start(()=>{
