f7ff681016d835b0c9819b4a67eab513c14baa59
Anwesh43 <anweshthecool0@gmail.com>
2017-12-16 23:40:11 +0530

    adding LineToCircle

--- line-to-circle.js
+++ line-to-circle.js
@@ -1,7 +1,7 @@
-const r = Math.min(window.innerWidth,window.innerHeight)
+const r = Math.min(window.innerWidth,window.innerHeight)/3
 class LineToCirclePoint {
     constructor(deg) {
-        this.deg = 0
+        this.deg = deg
         this.x = r*Math.cos(this.deg*Math.PI/180)
         this.y = r*Math.sin(this.deg*Math.PI/180)
     }
@@ -17,5 +17,37 @@ class LineToCirclePoint {
         else {
             this.x -= (2*Math.PI*r)/360
         }
+    }
+}
+class LineToCircle {
+    constructor() {
+        this.points = []
+        this.init()
+    }
+    init() {
+        for(var i=0;i<=360;i++) {
+            this.points.add(new LineToCirclePoint(i))
+        }
+    }
+    draw(context) {
+        context.save()
+        context.translate(r,r)
+        context.lineWidth = 5
+        context.beginPath()
+        this.points.forEach((point,index)=>{
+            if(index == 0) {
+                context.moveTo(point.x,point.y)
+            }
+            else {
+                context.lineTo(point.x,point.y)
+            }
+        })
+        context.stroke()
+        context.restore()
+    }
+    update() {
+        this.points.forEach((point)=>{
+            point.update()
+        })
     }
 }
